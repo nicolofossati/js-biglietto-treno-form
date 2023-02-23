8764d57c1f7f8fc0d26569236e96a998c1c25ff6
nicolo fossati <nicofossa01@gmail.com>
2023-02-23 17:01:33 +0100

    Progresso div che appare

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//  `
 let dom_name = document.getElementById("name_input");
 let dom_km = document.getElementById("km_input");
 let dom_age = document.getElementById("age_select");
@@ -15,6 +16,29 @@ send_bttn.addEventListener('click', function(){
     console.log(age);
 
     dom_riepilogo.classList.remove("d-none");
+
+
+    
+    let km_price = 0.21;
+    let discount_kp;
+    let offert;
+
+    if(age == "Minorenne"){
+        discount_kp = (km_price/100 *80); //sconto del 20%
+        price_value = (discount_kp * km).toFixed(2);
+        offert = "Sconto Minorenne";
+    } else if(age == "65+"){
+        discount_kp = (km_price/100 *60).toFixed(2); //sconto del 40%
+        price_value = discount_kp * km;
+        offert = "Sconto Silver";
+    } else {
+        price_value = (km_price * km).toFixed(2);
+        offert = "Biglietto Standard";
+    }
+
+    document.getElementById("name_r").innerHTML = name_surname;
+    document.getElementById("offert_r").innerHTML = offert;
+    document.getElementById("price_r").innerHTML = price_value;
 });
 
 const cancel_bttn = document.getElementById("ann_bttn");
@@ -26,3 +50,4 @@ cancel_bttn.addEventListener('click', function(){
 
     dom_riepilogo.classList.add("d-none");
 });
+
