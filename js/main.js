2c07e3116c2a2b3623f83b212a8bca4e1fa8364d
nicolo fossati <nicofossa01@gmail.com>
2023-02-23 16:33:46 +0100

    implementazione bottone reset

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+let dom_name = document.getElementById("name_input");
+let dom_km = document.getElementById("km_input");
+let dom_age = document.getElementById("age_select");
+
 const send_bttn = document.getElementById("gen_bttn");
 
 send_bttn.addEventListener('click', function(){
-    const name_surname = document.getElementById("name_input").value;
-    const km = document.getElementById("km_input").value;
-    const age = document.getElementById("age_select").value;
+    const name_surname = dom_name.value;
+    const km = dom_km.value;
+    const age = dom_age.value;
     console.log(name_surname);
     console.log(km);
     console.log(age);
 });
 
-const cancel_bttn = document.getElementById("gen_bttn");
+const cancel_bttn = document.getElementById("ann_bttn");
+
+cancel_bttn.addEventListener('click', function(){
+    dom_name.value = "";
+    dom_km.value = "";
+    dom_age.value = "";
+});
