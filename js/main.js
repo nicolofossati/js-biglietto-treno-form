4b47426b45075f36323d2d093fdc6396059e76cd
nicolo fossati <nicofossa01@gmail.com>
2023-02-23 15:59:55 +0100

    test eventListener

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const send_bttn = document.getElementById("gen_bttn");
+
+send_bttn.addEventListener('click', function(){
+    const km = document.getElementById("km_input").value;
+    const age = document.getElementById("age_select").value;
+    console.log(km);
+    console.log(age);
+});
+
