e2349059a9526446d43630c0cfd43aec80e2e805
nicolo fossati <nicofossa01@gmail.com>
2023-02-23 16:23:19 +0100

    MILESTONE 1 completo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const send_bttn = document.getElementById("gen_bttn");
 
 send_bttn.addEventListener('click', function(){
+    const name_surname = document.getElementById("name_input").value;
     const km = document.getElementById("km_input").value;
     const age = document.getElementById("age_select").value;
+    console.log(name_surname);
     console.log(km);
     console.log(age);
 });
 
+const cancel_bttn = document.getElementById("gen_bttn");
